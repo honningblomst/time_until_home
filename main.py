2fab49f6efa6d387a3e4ed633ac5dbcbd044792e
Herman <hermanhoier@gmail.com>
2024-11-12 14:33:29 +0100

    Added seconds
    Lagt til sekunder i nedtellingen, samt fikset så den displayer riktig sekunder ifht hvor hvor mange sekunder det er igjen

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#TODO variabel for når jeg vil hjem
-#TODO variabel for hva tiden er
-#TODO noe kalkulasjon for tiden jeg vil hjem - tiden nå
-#TODO print som forteller hvor lenge det er til hjem
 from datetime import datetime
 
 # Getting the time now
@@ -10,23 +6,31 @@ time_now = datetime.now()
 # Getting the hour and minute
 hour = time_now.hour
 minute = time_now.minute
+seconds = time_now.second
 
 # User input for the time they want to go home
 home_hour = int(input("What time do you want to go home? (hour): "))
 home_minute = int(input("What time do you want to go home? (minute): "))
+home_seconds = int(input("What time do you want to go home? (seconds): "))
 
 def calculate_home():
     # Calculate hours and minutes until home
     until_home_hour = home_hour - hour
     until_home_minute = home_minute - minute
+    until_home_seconds = home_seconds - seconds
 
     # Adjust if minutes are negative
     if until_home_minute < 0:
         until_home_minute += 60
         until_home_hour -= 1
 
+    if until_home_seconds < 0:
+        until_home_seconds += 60
+        until_home_minute -= 1
+
     # Print the result
-    return print(f"It is sadly {until_home_hour}:{until_home_minute:02} until home :-(")
+    return print(f"It is sadly {until_home_hour} hours, {until_home_minute:02} minutes"
+                 f" and {until_home_seconds} seconds until home :-(")
 
 print()
 calculate_home()
